9d8764e54edb0889017d8d79968ca7b63f878fd8
Mazzinii <luizeduardomazzini@gmail.com>
2024-05-13 18:17:37 -0300

    projeto da faculdade

--- script.js
+++ script.js
@@ -7,8 +7,14 @@ const filhos = document.querySelector(".nav-contemfilhos"); //pegando o elemento
 const pai1 = document.querySelector(".pai1"); //pegando o elemento pai
 const filhos1 = document.querySelector(".nav-contemfilhos1"); //pegando o elemento filho
 
+/* --------FORMULARIO------  */
+
+const buttonForm = document.querySelector(".button-curso");
+
+/* --------MENU HAMBURGUER------  */
 hamburguer.addEventListener("click", () => nav.classList.toggle("active")); //adicionando evento de clique que quando ativo mostra o menu nav
 
-pai.addEventListener("click", () => filhos.classList.toggle("active"));
+/* --------SUBMENUS------  */
+pai.addEventListener("click", () => filhos.classList.toggle("active")); //adicionando evento de clique que quando ativo mostrar o primeiro submenu
 
-pai1.addEventListener("click", () => filhos1.classList.toggle("active"));
+pai1.addEventListener("click", () => filhos1.classList.toggle("active")); //adicionando evento de clique que quando ativo mostrar o segundo submenu
