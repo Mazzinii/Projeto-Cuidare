f3e50b5defda1f665b427391038ec7a48e9ddc60
Mazzinii <luizeduardomazzini@gmail.com>
2024-05-15 16:49:10 -0300

    thanks page

--- script.js
+++ script.js
@@ -6,6 +6,18 @@ const filhos = document.querySelector(".nav-contemfilhos"); //pegando o elemento
 
 const pai1 = document.querySelector(".pai1"); //pegando o elemento pai
 const filhos1 = document.querySelector(".nav-contemfilhos1"); //pegando o elemento filho
+/* --------FORMULARIO------  */
+const button = document.querySelector(".button-curso");
+
+button.addEventListener("click", function () {
+  button.innerText = "Enviando...";
+  button.style.backgroundColor = "grey";
+  button.preventDefault();
+});
+
+function thanks() {
+  window.open("https://institutocuidare.netlify.app/thanks", "_top");
+}
 
 /* --------MENU HAMBURGUER------  */
 hamburguer.addEventListener("click", () => nav.classList.toggle("active")); //adicionando evento de clique que quando ativo mostra o menu nav
